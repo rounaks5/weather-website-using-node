d6c1627503f63f5ee0739d6395e0adc32328f33a
rounak <ronakshah9522@gmail.com>
2020-03-12 14:34:13 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,100 @@
+const path=require('path')
+const express=require("express")
+const hbs=require("hbs")
+
+const geocode = require("../src/utils/geocode.js")
+const forecast = require("../src/utils/forecast.js")
+
+//console.log(__dirname)
+//console.log(__filename)
+//console.log(path.join(__dirname,'../public'))
+
+const app = express()
+
+//Define Paths for express Config
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewPath =path.join(__dirname,'../templates/views')
+const partialPath =path.join(__dirname,'../templates/partials')
+
+//Setup handlebars engine & Views Location
+app.set('view engine','hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialPath)
+
+//set up Static Directory to serve
+app.use(express.static(publicDirectoryPath))
+
+//Page Routes created for index, about, help page
+app.get('', (req,res)=>{
+	res.render("index",{
+		title:"Weather App",
+		name: "Rounak Shah"
+	})
+})
+
+app.get('/about',(req,res)=>{
+	res.render("about",{
+		title: "About me",
+		name: "Rounak Shah"
+	})
+})
+
+app.get("/help",(req,res)=>{
+	res.render("help",{
+		helpText:"This is helpful text",
+		title:"Help",
+		name:"Rounak Shah"
+	})
+})
+
+
+//Below 3 lines only works if we have set root index page path using app.use()
+// app.get('', (req,res)= >{
+// 	res.send('<h1>Weather</h1>')
+// })
+app.get('/weather', (req,res)=>{
+	if(!req.query.address){
+		return res.send({
+			error : "You must Provide an address term"
+		})
+	}
+
+	geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+		if(error){
+		return res.send({error})
+			}
+		forecast(latitude,longitude, (error, forecastData) => {
+	  		if(error){
+				return res.send({error})
+				}
+			res.send({
+				foreCast : forecastData,
+				Location : location,
+				address : req.query.address
+			})
+		})
+
+	})
+	
+})
+ 
+app.get('/help/*',(req,res)=>{
+	res.render('error',{
+		error:"help article not found",
+		title:"404",
+		name:"Rounak Shah"
+	})
+})
+
+app.get('*',(req,res)=>{
+	res.render('error',{
+		error:"Page Not found",
+		title:"404",
+		name:"Rounak Shah"
+	})
+})
+
+app.listen(3000,()=>{
+	console.log('Server is up on Port 3000')
+})
+ 
---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+const forecast =(lat,long,callback) =>{
+	const url = "https://api.darksky.net/forecast/50955ca28f225d827b0f63fca20394aa/"+lat+","+long+"?units=si"
+	request({url,json:true},(error,{body})=>{
+		if(error)
+		{
+			callback("Unable to connect to weather Services",undefined)
+		}
+		else if(body.error){
+			callback("Unable to find location",undefined)
+		}
+		else{
+			text =body.daily.data[0].summary + ' It is currently ' + body.currently.temperature +' degree out'
+			callback(undefined,text)
+		}
+
+	})
+
+}
+
+module.exports = forecast
